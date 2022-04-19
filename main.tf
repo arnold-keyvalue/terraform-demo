93860fc7ff82ae58e7b838a37536ab2bcf3bb181
arnold-keyvalue <arnold@keyvalue.systems>
2022-04-19 13:06:02 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -1,17 +1,2 @@
-module "tf-session-bucket1" {
-  source = "./modules/s3"
-  name   = "tf-session-bucket1"
-  acl    = "private"
-}
 
-module "tf-session-bucket2" {
-  source = "./modules/s3"
-  name   = "tf-session-bucket2"
-  acl    = "private"
-}
 
-module "tf-session-bucket3" {
-  source = "./modules/s3"
-  name   = "tf-session-bucket3"
-  acl    = "private"
-}
